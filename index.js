fb65c49d1970c5030742142a81a105f289d64517
Hao Yan <yanok9812@gmail.com>
2021-12-07 17:02:38 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+  host: "housingdata.c0kxq0umbaz7.us-west-2.rds.amazonaws.com",
+  user: "admin",
+  password: "12345678",
+  database: "housingData"
+})
+con.connect()
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    con.query("SELECT * FROM properties", function (err, result, fields) {
+      if (err) throw err;
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(JSON.stringify(result));
+    });
+});
+
+server.listen(port, hostname, () => {});
